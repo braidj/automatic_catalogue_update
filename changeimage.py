ff05fc3d5d9d20445b2cdd6f83ea2a220b3ed6fd
Jason Braid <braidj@gmail.com>
2020-08-23 16:49:20 +0100

    Convert to RGB and resize images

--- changeimage.py
+++ changeimage.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
+import os
+from PIL import Image
 
-print("in progress")
+IMAGES_DIR = "supplier-data/images"
+
+if __name__ == "__main__":
+    images = [x for x in os.listdir(IMAGES_DIR) if 'tiff' in x] # only want .tiff
+
+    for img in images:
+        source = r"./{}/{}".format(IMAGES_DIR, img)
+        tgt_name = "{}.JPEG".format(source[:-5])
+
+        try:
+            current = Image.open(source)
+            new_image = current.convert('RGB')
+            new_image = new_image.resize((600, 400))
+            new_image.save(tgt_name)
+            print(f"Converted {tgt_name}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    print("Done")
