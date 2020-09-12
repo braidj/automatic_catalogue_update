0eb9cc32c467e1ee2ac0603d093855fe6fe2b1f0
Jason Braid <braidj@gmail.com>
2020-09-12 16:25:04 +0100

    main script

--- run.py
+++ run.py
@@ -4,22 +4,19 @@ import os
 import json
 
 
-def get_json(source):
+def post_json_file(source):
 
     txt_name = os.path.basename(source)[:7]
     image_name=txt_name.replace('.txt','.jpeg')
     json_name = txt_name.replace('.txt','.json')
+    url = "http://localhost/upload/" # change to the correct place
 
-   # template = {"name": "Watermelon", "weight": 500, "description": "Watermelon is good for relieving heat, eliminating annoyance and quenching thirst. It contains a lot of water, which is good for relieving the symptoms of acute fever immediately. The sugar and salt contained in watermelon can diuretic and eliminate kidney inflammation. Watermelon also contains substances that can lower blood pressure.", "image_name": "010.jpeg"}
+    print(f"\nGoing to post {json_name} {url}")
+
     with open(source,'r') as desc_details:
+
         raw_lines = desc_details.readlines()
         clean_lines = [x.strip() for x in raw_lines]
-
-        #print(clean_lines)
-        print(f"\nimage_name:{image_name}")
-        print(f"name=:{clean_lines[0]}")
-        print(f"weight=:{clean_lines[1][:-4]}")
-        print(f"description=:{clean_lines[2]}\n")
 
         result = {}
         result['name'] = clean_lines[0]
@@ -32,6 +29,9 @@ def get_json(source):
         with open(json_name, 'w') as fp:
             json.dump(result, fp)
 
+        # with open(json_name, 'rb') as opened:
+        #     r = requests.post(url, files={'file': })
+
 
 
 if __name__ == "__main__":
@@ -41,11 +41,6 @@ if __name__ == "__main__":
 
     desc_files = [x for x in os.listdir(DESC_DIR) if 'txt' in x] # only want .txt
 
-    url = "http://localhost/upload/"
-
     for f in desc_files:
         source = r"{}/{}".format(DESC_DIR, f)
-        get_json(source)
-
-        # with open(source, 'rb') as opened:
-        #     r = requests.post(url, files={'file': opened})
+        post_json_file(source)
